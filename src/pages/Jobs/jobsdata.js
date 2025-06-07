5f1afea9d568d0cd02ea82582dd8ca61f7aa0075
Byreddy Rajesh <rajeshbyreddy@Byreddys-MacBook-Air.local>
2025-06-07 17:59:00 +0530

    updated new job

--- src/pages/Jobs/jobsdata.js
+++ src/pages/Jobs/jobsdata.js
@@ -1,6 +1,6 @@
 export const jobsdata = [
   {
-    job_id: 6572683,
+     job_id: 6572683,
     tags: ['software-engineer', 'internship', 'backend'],
     skills: ['Node.js', 'Python', 'Kafka', 'APIs', 'REST', 'JSON', 'Event-driven systems'],
     qualifications: [
@@ -18,4 +18,26 @@ export const jobsdata = [
     applyLink: 'https://jobs.lever.co/goodleap/286f7562-212b-4c37-9265-c8555577c1fc?lever-source=Indeed', // Placeholder, update if needed
     salary: '$20 - $25 an hour',
     image: 'https://lever-client-logos.s3.us-west-2.amazonaws.com/bbd022e2-ead3-484c-a9d4-ea37530cf3bd-1682439634140.png' // Using official logo URL or placeholder
-  }]
+  },{
+    job_id: 6572684,
+    tags: ['software-engineer', 'full-time', 'embedded'],
+    skills: [
+      'C/C++', 'Embedded Systems', 'Android', 'Linux', 'Multimedia', 'Wireless Protocols',
+      'Device Drivers', 'Kernel Development', 'Object-Oriented Design'
+    ],
+    qualifications: [
+      "Bachelor's or Master's degree in Computer Science, Electronics and Communication Engineering, or related fields",
+      'Strong understanding of OS concepts and data structures',
+      'Knowledge of wireless standards like 4G, WiFi, Bluetooth',
+      'Familiarity with multimedia technologies (Audio, Video, Imaging)',
+      'Good communication, analytical and problem-solving skills'
+    ],
+    about:
+      'Qualcomm is hiring Software Engineers for various domains including Multimedia, Modem, Linux Kernel, IOT, and more. Roles involve developing embedded software and drivers, wireless protocol stacks, platform software, and application UIs for Qualcomm chipsets.',
+    companyName: 'Qualcomm India Private Limited',
+    roleName: 'Software Engineer',
+    applyLink: 'https://careers.qualcomm.com/careers/job/446700670232',
+    salary: 'Not Disclosed',
+    image: 'https://assets.weforum.org/organization/image/RLbuGj8AsF1yZ1w665My01JM8jy27_VaE6gmokbHi8U.jpg'
+  }
+]
